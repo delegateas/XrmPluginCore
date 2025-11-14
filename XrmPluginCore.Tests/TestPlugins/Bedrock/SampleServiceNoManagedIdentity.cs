09ee881868e5e7d5d3d8b058e249ac5889e30a1d
Magnus Gether Sørensen (Delegate) <mso@delegate.dk>
2025-11-14 10:02:06 +0100

    Added comprehensive tests

---
+++ XrmPluginCore.Tests/TestPlugins/Bedrock/SampleServiceNoManagedIdentity.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Xrm.Sdk;
+
+namespace XrmPluginCore.Tests.TestPlugins.Bedrock
+{
+    public class SampleServiceNoManagedIdentity : ISampleService
+    {
+        public bool HandleCreateCalled { get; private set; }
+        public IPluginExecutionContext PluginContext { get; }
+        public IOrganizationService OrganizationService { get; }
+
+        public SampleServiceNoManagedIdentity(IPluginExecutionContext pluginContext, IOrganizationServiceFactory organizationServiceFactory)
+        {
+            PluginContext = pluginContext;
+            OrganizationService = organizationServiceFactory.CreateOrganizationService(pluginContext.UserId);
+        }
+
+        public void HandleCreate()
+        {
+            HandleCreateCalled = true;
+        }
+    }
+}
