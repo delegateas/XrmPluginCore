ea669f41514f96712f8ede27d3b69542182824cc
Morten Holt <mkh@contextand.com>
2025-11-28 13:03:01 +0100

    FIX: Accidentally removed using statements that where needed

--- XrmPluginCore.SourceGenerator/Analyzers/HandlerSignatureMismatchAnalyzer.cs
+++ XrmPluginCore.SourceGenerator/Analyzers/HandlerSignatureMismatchAnalyzer.cs
@@ -1,3 +1,9 @@
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+using System.Collections.Immutable;
+using System.Linq;
 using XrmPluginCore.SourceGenerator.Helpers;
 
 namespace XrmPluginCore.SourceGenerator.Analyzers;
